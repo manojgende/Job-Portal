eeed12a99b5b94f87f30e16baeedcb7501725860
manoj gowda hr <manojgende2513@gmail.com>
2025-05-14 22:12:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+app.use(cors({
+  origin: "https://job-portal-frontend.vercel.app",
+  credentials: true
+}));
 
 app.use(cors(corsOptions));
 
